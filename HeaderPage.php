00643e4b12db114a7a7c11c8571ac89185d3641e
NeumontJack <100381502+NeumontJack@users.noreply.github.com>
2024-05-02 11:28:00 -0600

    Update

--- HeaderPage.php
+++ HeaderPage.php
@@ -5,9 +5,12 @@ $PageTitle = "";
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title></title>
-    <link rel="stylesheet" href="StyleSheet.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Include your CSS file -->
 </head>
 <body>
